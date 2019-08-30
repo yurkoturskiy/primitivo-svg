ea4e6bd8fb560eb35cc88a8d14d8040e94f9c8f4
guandjoy <guandjoy@gmail.com>
2019-08-30 16:52:12 +0300

    Add phasesLayer to root file

--- src/index.js
+++ src/index.js
@@ -14,10 +14,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var index_1 = __importDefault(require("./path/index"));
 var index_2 = __importDefault(require("./morphing/index"));
 var index_3 = __importDefault(require("./spacing/index"));
+var index_4 = __importDefault(require("./phases/index"));
 var log = __importStar(require("loglevel"));
-log.setLevel("info");
+log.setLevel("warn");
 exports.path = function (parameters) { return index_1.default(parameters); };
 exports.morphing = function (animateParameters, keyPathsParameters) {
     return index_2.default(animateParameters, keyPathsParameters);
 };
 exports.spacing = function (parameters) { return index_3.default(parameters); };
+exports.phases = function (parameters) { return index_4.default(parameters); };
--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,20 @@
 import pathLayer from "./path/index";
 import morphingLayer from "./morphing/index";
 import spacingLayer from "./spacing/index";
+import phasesLayer from "./phases/index";
 // Interfaces
-import { Parameters, PathData } from "./path/interfaces";
+import { InputParameters, PathData } from "./path/interfaces";
 import { AnimateParameters, KeyPathParameters } from "./morphing/interfaces";
 
 import * as log from "loglevel";
 
-log.setLevel("info");
+log.setLevel("warn");
 
 export const path = (parameters: any): PathData => pathLayer(parameters);
 
-export const morphing = (animateParameters: any, keyPathsParameters: any) =>
+export const morphing = (animateParameters: any, keyPathsParameters?: any) =>
   morphingLayer(animateParameters, keyPathsParameters);
 
 export const spacing = (parameters: any) => spacingLayer(parameters);
+
+export const phases = (parameters: any) => phasesLayer(parameters);
