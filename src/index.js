5d59e34266bd1f6b23a984d39b68910b1e76fc24
guandjoy <guandjoy@gmail.com>
2019-09-09 19:04:51 +0300

    Add randomRange function to the root index file

--- src/index.js
+++ src/index.js
@@ -8,14 +8,25 @@ var index_1 = __importDefault(require("./path/index"));
 var index_2 = __importDefault(require("./morphing/index"));
 var index_3 = __importDefault(require("./spacing/index"));
 var index_4 = __importDefault(require("./phases/index"));
+// Misc
+var index_5 = require("./misc/index");
+///////////////////
+// Logging setup //
 var log = require("loglevel");
 log.setLevel("warn");
 log.getLogger("path-log").setLevel("warn");
 log.getLogger("phases-log").setLevel("warn");
 log.getLogger("spacing-log").setLevel("warn");
+////////////
+// Layers //
 exports.path = function (parameters) { return index_1.default(parameters); };
 exports.morphing = function (animateParameters, keyPathsParameters) {
     return index_2.default(animateParameters, keyPathsParameters);
 };
 exports.spacing = function (parameters) { return index_3.default(parameters); };
 exports.phases = function (parameters) { return index_4.default(parameters); };
+////////////////////
+// Misc functions //
+exports.randomRange = function (min, max) {
+    return index_5.randomRange(min, max);
+};
--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,22 @@ import pathLayer from "./path/index";
 import morphingLayer from "./morphing/index";
 import spacingLayer from "./spacing/index";
 import phasesLayer from "./phases/index";
+// Misc
+import { randomRange as miscRandomRange } from "./misc/index";
 // Interfaces
 import { InputParameters, PathData } from "./path/interfaces";
 import { AnimateParameters, KeyPathParameters } from "./morphing/interfaces";
 
+///////////////////
+// Logging setup //
 var log = require("loglevel");
 log.setLevel("warn");
 log.getLogger("path-log").setLevel("warn");
 log.getLogger("phases-log").setLevel("warn");
 log.getLogger("spacing-log").setLevel("warn");
 
+////////////
+// Layers //
 export const path = (parameters: any): PathData => pathLayer(parameters);
 
 export const morphing = (animateParameters: any, keyPathsParameters?: any) =>
@@ -21,3 +27,8 @@ export const morphing = (animateParameters: any, keyPathsParameters?: any) =>
 export const spacing = (parameters: any) => spacingLayer(parameters);
 
 export const phases = (parameters: any) => phasesLayer(parameters);
+
+////////////////////
+// Misc functions //
+export const randomRange = (min: number, max: number): number =>
+  miscRandomRange(min, max);
