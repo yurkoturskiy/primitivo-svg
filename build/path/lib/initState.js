94634ad04be7466888dd71bdad87a268708a1c2d
Yurko Turskiy <guandjoy@gmail.com>
2020-02-08 23:59:35 +0200

    Separate initState

---
+++ build/path/lib/initState.js
@@ -0,0 +1,23 @@
+"use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var defaultParameters_1 = __importDefault(require("./defaultParameters"));
+var ramda_1 = require("ramda");
+var setDefaultParams = function (parameters) { return (__assign({}, defaultParameters_1.default, parameters, { numOfGroups: parameters.groups.length, groups: parameters.groups.map(function (group) { return (__assign({}, defaultParameters_1.default.groups[0], group)); }) })); };
+var createPath = function (parameters) { return ({
+    parameters: parameters
+}); };
+var initState = ramda_1.pipe(setDefaultParams, createPath);
---
+++ src/path/lib/initState.ts
@@ -0,0 +1,20 @@
+import defaultParameters from "./defaultParameters";
+import { pipe } from "ramda";
+// Interfaces
+import { PathData, InputParameters } from "../interfaces";
+
+const setDefaultParams = (parameters: InputParameters): InputParameters => ({
+  ...defaultParameters,
+  ...parameters,
+  numOfGroups: parameters.groups.length,
+  groups: parameters.groups.map(group => ({
+    ...defaultParameters.groups[0],
+    ...group
+  }))
+});
+
+const createPath = (parameters: InputParameters): PathData => ({
+  parameters
+});
+
+const initState = pipe(setDefaultParams, createPath);
