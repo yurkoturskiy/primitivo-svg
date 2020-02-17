92d80c8b8d177a3aacaf50c6b266366a96ae610b
Yurko Turskiy <guandjoy@gmail.com>
2020-02-17 23:54:34 +0200

    Refactor setArms

--- build/path/index.js
+++ build/path/index.js
@@ -1,15 +1,4 @@
 "use strict";
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -21,41 +10,12 @@ var parseGroupParameter_1 = __importDefault(require("./lib/parseGroupParameter")
 var setFrame_1 = __importDefault(require("./lib/setFrame"));
 var generateVertexes_1 = __importDefault(require("./lib/generateVertexes"));
 var remapVertexes_1 = __importDefault(require("./lib/remapVertexes"));
-// Logging
+var setArms_1 = __importDefault(require("./lib/setArms"));
+// logging
 var log = require("loglevel").getLogger("path-log");
 /***********
  * Methods *
  ***********/
-var getSmartRoundValue = function (group, vertexIndex) {
-    var parameter = group.smartRound;
-    parameter = parseGroupParameter_1.default(parameter, vertexIndex);
-    if (!parameter)
-        return parameter;
-    else if (typeof parameter !== "boolean")
-        throw "Wrong 'smartRound' parameter in group number " + group.pk;
-    else
-        return parameter;
-};
-var getLengthBasedRoundValue = function (group, vertexIndex) {
-    var parameter = group.lengthBasedRound;
-    parameter = parseGroupParameter_1.default(parameter, vertexIndex);
-    if (!parameter)
-        return parameter;
-    else if (typeof parameter !== "boolean")
-        throw "Wrong 'lengthBasedRound' parameter in group number " + group.pk;
-    else
-        return parameter;
-};
-var getAdaptArmsValue = function (group, vertexIndex) {
-    var parameter = group.adaptArms;
-    parameter = parseGroupParameter_1.default(parameter, vertexIndex);
-    if (!parameter)
-        return parameter;
-    else if (typeof parameter !== "boolean")
-        throw "Wrong 'adaptArms' parameter in group number " + group.pk;
-    else
-        return parameter;
-};
 var getRadiansValue = function (group, vertexIndex) {
     var parameter = group.radians;
     parameter = parseGroupParameter_1.default(parameter, vertexIndex);
@@ -65,108 +25,6 @@ var getRadiansValue = function (group, vertexIndex) {
         throw "Wrong 'radians' parameter in group number " + group.pk;
     else
         return parameter;
-};
-var setArms = function (mode, path) {
-    var vertexes = path.vertexes;
-    var _a = path.parameters, groups = _a.groups, averageLength = _a.averageLength;
-    var numOfPoints = vertexes.length - 1; // Minus "M" vertex
-    var averageLength;
-    for (var i = 1; i < vertexes.length; i++) {
-        // Adapt arms
-        var firstArmAdapt = getAdaptArmsValue(groups[vertexes[i - 1].group], vertexes[i - 1].indexWithingGroup);
-        var secondArmAdapt = getAdaptArmsValue(groups[vertexes[i].group], vertexes[i].indexWithingGroup);
-        if (mode === "init" && firstArmAdapt && secondArmAdapt)
-            continue;
-        else if (mode === "adapt" && !firstArmAdapt && !secondArmAdapt)
-            continue;
-        // Prepare vars
-        var firstArmLength = void 0, secondArmLength = void 0;
-        // Smart round
-        var firstArmSmartRound = getSmartRoundValue(groups[vertexes[i - 1].group], vertexes[i - 1].indexWithingGroup);
-        var secondArmSmartRound = getSmartRoundValue(groups[vertexes[i].group], vertexes[i].indexWithingGroup);
-        // Length based round
-        var firstArmLengthBasedRound = getLengthBasedRoundValue(groups[vertexes[i - 1].group], vertexes[i - 1].indexWithingGroup);
-        var secondArmLengthBasedRound = getLengthBasedRoundValue(groups[vertexes[i].group], vertexes[i].indexWithingGroup);
-        // Calc individual factor for smart round
-        var individualFactor = void 0;
-        if (firstArmSmartRound || secondArmSmartRound) {
-            var distanceRadians = index_1.radiansDelta(vertexes[i - 1].radians, vertexes[i].radians);
-            individualFactor = (2 * Math.PI) / distanceRadians;
-        }
-        // First arm
-        if ((mode === "adapt" && firstArmAdapt) ||
-            (mode === "init" && !firstArmAdapt)) {
-            // Calc first arm
-            log.info("calc first arm. Mode: " + mode);
-            var firstArmFactor = firstArmSmartRound ? individualFactor : numOfPoints;
-            firstArmLength = (4 / 3) * Math.tan(Math.PI / (2 * firstArmFactor));
-            if (mode === "adapt") {
-                // Set scale
-                var firstArmScaleFactor = firstArmLengthBasedRound
-                    ? vertexes[i - 1].length
-                    : averageLength;
-                firstArmLength *= firstArmScaleFactor;
-            }
-            // Round
-            firstArmLength *= vertexes[i - 1].round[1];
-            // Set angle
-            var firstArmRadians = vertexes[i - 1].radians + Math.PI / 2; // angle + 90 from the previous point angle
-            var firstArmAngle = index_1.radToAngle(firstArmRadians);
-            log.debug("first arm angle", firstArmAngle);
-            // Set cos and sin
-            var cosx1 = index_1.round(Math.cos(firstArmRadians));
-            if (mode === "adapt")
-                cosx1 *= -1;
-            var siny1 = index_1.round(Math.sin(firstArmRadians));
-            // Set coordinates
-            var x1 = cosx1 * firstArmLength + vertexes[i - 1].x;
-            var y1 = siny1 * firstArmLength + vertexes[i - 1].y;
-            log.debug("vertex " + i + " first arm x: " + x1 + " y: " + y1);
-            // Add to vertex
-            vertexes[i] = __assign({}, vertexes[i], { x1: x1,
-                y1: y1,
-                cosx1: cosx1,
-                siny1: siny1 });
-        }
-        // Second arm
-        if ((mode === "adapt" && secondArmAdapt) ||
-            (mode === "init" && !secondArmAdapt)) {
-            // Calc second arm
-            log.info("calc second arm. Mode: " + mode);
-            var secondArmFactor = secondArmSmartRound
-                ? individualFactor
-                : numOfPoints;
-            secondArmLength = (4 / 3) * Math.tan(Math.PI / (2 * secondArmFactor));
-            if (mode === "adapt") {
-                // Set scale
-                var secondArmScaleFactor = secondArmLengthBasedRound
-                    ? vertexes[i].length
-                    : averageLength;
-                secondArmLength *= secondArmScaleFactor;
-            }
-            // Set round
-            secondArmLength *= vertexes[i].round[0];
-            // Set angle
-            var secondArmRadians = vertexes[i].radians - Math.PI / 2; // angle + 90 from cur point
-            var secondArmAngle = index_1.radToAngle(secondArmRadians);
-            log.debug("second arm angle", secondArmAngle);
-            // Set cos and sin
-            var cosx2 = index_1.round(Math.cos(secondArmRadians));
-            if (mode === "adapt")
-                cosx2 *= -1;
-            var siny2 = index_1.round(Math.sin(secondArmRadians));
-            // Set coordinates
-            var x2 = cosx2 * secondArmLength + vertexes[i].x;
-            var y2 = siny2 * secondArmLength + vertexes[i].y;
-            log.debug("vertex " + i + " second arm x: " + x2 + " y: " + y2);
-            // Add to vertex
-            vertexes[i] = __assign({}, vertexes[i], { x2: x2,
-                y2: y2,
-                cosx2: cosx2,
-                siny2: siny2 });
-        }
-    }
-    return path;
 };
 var getIncircleValue = function (group, vertexIndex) {
     var parameter = group.incircle;
@@ -447,7 +305,7 @@ var pathLayer = function (parameters) {
     path = setFrame_1.default(path);
     path = generateVertexes_1.default(path);
     path = remapVertexes_1.default(path); // Add M point
-    path = setArms("init", path);
+    path = setArms_1.default("init", path);
     path = scaleToOne(path);
     path = setCenter(path);
     path = setDistance(path);
@@ -457,7 +315,7 @@ var pathLayer = function (parameters) {
     path = setLength(path);
     path = calcLength(path);
     path = recalcRadians(path);
-    path = setArms("adapt", path);
+    path = setArms_1.default("adapt", path);
     path = shift(path);
     path = generateD(path);
     return path;
---
+++ src/path/lib/setArms.ts
@@ -0,0 +1,179 @@
+import { PathData, GroupParameters } from "../interfaces";
+import parseGroupParameter from "./parseGroupParameter";
+import { radiansDelta, radToAngle, round } from "../../misc";
+
+// logging
+const log = require("loglevel").getLogger("path-log");
+
+const getAdaptArmsValue = (
+  group: GroupParameters,
+  vertexIndex: number
+): boolean => {
+  let parameter: any = group.adaptArms;
+  parameter = parseGroupParameter(parameter, vertexIndex);
+  if (!parameter) return parameter;
+  else if (typeof parameter !== "boolean")
+    throw `Wrong 'adaptArms' parameter in group number ${group.pk}`;
+  else return parameter;
+};
+
+const getSmartRoundValue = (
+  group: GroupParameters,
+  vertexIndex: number
+): boolean => {
+  let parameter: any = group.smartRound;
+  parameter = parseGroupParameter(parameter, vertexIndex);
+  if (!parameter) return parameter;
+  else if (typeof parameter !== "boolean")
+    throw `Wrong 'smartRound' parameter in group number ${group.pk}`;
+  else return parameter;
+};
+
+const getLengthBasedRoundValue = (
+  group: GroupParameters,
+  vertexIndex: number
+): boolean => {
+  let parameter: any = group.lengthBasedRound;
+  parameter = parseGroupParameter(parameter, vertexIndex);
+  if (!parameter) return parameter;
+  else if (typeof parameter !== "boolean")
+    throw `Wrong 'lengthBasedRound' parameter in group number ${group.pk}`;
+  else return parameter;
+};
+
+const setArms = (mode: string, path: PathData): PathData => {
+  var { vertexes } = path;
+  var { groups, averageLength } = path.parameters;
+  var numOfPoints = vertexes.length - 1; // Minus "M" vertex
+  var averageLength: number;
+  for (let i = 1; i < vertexes.length; i++) {
+    // Adapt arms
+    let firstArmAdapt = getAdaptArmsValue(
+      groups[vertexes[i - 1].group],
+      vertexes[i - 1].indexWithingGroup
+    );
+    let secondArmAdapt = getAdaptArmsValue(
+      groups[vertexes[i].group],
+      vertexes[i].indexWithingGroup
+    );
+
+    if (mode === "init" && firstArmAdapt && secondArmAdapt) continue;
+    else if (mode === "adapt" && !firstArmAdapt && !secondArmAdapt) continue;
+
+    // Prepare vars
+    let firstArmLength, secondArmLength;
+    // Smart round
+    let firstArmSmartRound = getSmartRoundValue(
+      groups[vertexes[i - 1].group],
+      vertexes[i - 1].indexWithingGroup
+    );
+    let secondArmSmartRound = getSmartRoundValue(
+      groups[vertexes[i].group],
+      vertexes[i].indexWithingGroup
+    );
+    // Length based round
+    let firstArmLengthBasedRound = getLengthBasedRoundValue(
+      groups[vertexes[i - 1].group],
+      vertexes[i - 1].indexWithingGroup
+    );
+    let secondArmLengthBasedRound = getLengthBasedRoundValue(
+      groups[vertexes[i].group],
+      vertexes[i].indexWithingGroup
+    );
+
+    // Calc individual factor for smart round
+    let individualFactor;
+    if (firstArmSmartRound || secondArmSmartRound) {
+      let distanceRadians = radiansDelta(
+        vertexes[i - 1].radians,
+        vertexes[i].radians
+      );
+      individualFactor = (2 * Math.PI) / distanceRadians;
+    }
+
+    // First arm
+    if (
+      (mode === "adapt" && firstArmAdapt) ||
+      (mode === "init" && !firstArmAdapt)
+    ) {
+      // Calc first arm
+      log.info(`calc first arm. Mode: ${mode}`);
+      let firstArmFactor = firstArmSmartRound ? individualFactor : numOfPoints;
+      firstArmLength = (4 / 3) * Math.tan(Math.PI / (2 * firstArmFactor));
+      if (mode === "adapt") {
+        // Set scale
+        let firstArmScaleFactor = firstArmLengthBasedRound
+          ? vertexes[i - 1].length
+          : averageLength;
+        firstArmLength *= firstArmScaleFactor;
+      }
+      // Round
+      firstArmLength *= vertexes[i - 1].round[1];
+      // Set angle
+      let firstArmRadians = vertexes[i - 1].radians + Math.PI / 2; // angle + 90 from the previous point angle
+      let firstArmAngle = radToAngle(firstArmRadians);
+      log.debug("first arm angle", firstArmAngle);
+      // Set cos and sin
+      let cosx1 = round(Math.cos(firstArmRadians));
+      if (mode === "adapt") cosx1 *= -1;
+      let siny1 = round(Math.sin(firstArmRadians));
+      // Set coordinates
+      let x1 = cosx1 * firstArmLength + vertexes[i - 1].x;
+      let y1 = siny1 * firstArmLength + vertexes[i - 1].y;
+      log.debug(`vertex ${i} first arm x: ${x1} y: ${y1}`);
+      // Add to vertex
+      vertexes[i] = {
+        ...vertexes[i],
+        x1,
+        y1,
+        cosx1,
+        siny1
+      };
+    }
+
+    // Second arm
+    if (
+      (mode === "adapt" && secondArmAdapt) ||
+      (mode === "init" && !secondArmAdapt)
+    ) {
+      // Calc second arm
+      log.info(`calc second arm. Mode: ${mode}`);
+      let secondArmFactor = secondArmSmartRound
+        ? individualFactor
+        : numOfPoints;
+      secondArmLength = (4 / 3) * Math.tan(Math.PI / (2 * secondArmFactor));
+      if (mode === "adapt") {
+        // Set scale
+        let secondArmScaleFactor = secondArmLengthBasedRound
+          ? vertexes[i].length
+          : averageLength;
+        secondArmLength *= secondArmScaleFactor;
+      }
+      // Set round
+      secondArmLength *= vertexes[i].round[0];
+      // Set angle
+      let secondArmRadians = vertexes[i].radians - Math.PI / 2; // angle + 90 from cur point
+      let secondArmAngle = radToAngle(secondArmRadians);
+      log.debug("second arm angle", secondArmAngle);
+      // Set cos and sin
+      let cosx2 = round(Math.cos(secondArmRadians));
+      if (mode === "adapt") cosx2 *= -1;
+      let siny2 = round(Math.sin(secondArmRadians));
+      // Set coordinates
+      let x2 = cosx2 * secondArmLength + vertexes[i].x;
+      let y2 = siny2 * secondArmLength + vertexes[i].y;
+      log.debug(`vertex ${i} second arm x: ${x2} y: ${y2}`);
+      // Add to vertex
+      vertexes[i] = {
+        ...vertexes[i],
+        x2,
+        y2,
+        cosx2,
+        siny2
+      };
+    }
+  }
+  return path;
+};
+
+export default setArms;
