702b75e4da3dbc4b1b0b9526e3b014fc04b4dcc8
Yurko Turskiy <guandjoy@gmail.com>
2020-04-10 23:58:57 +0300

    Create setDefaultKeyTimes.ts
    Refactor prepareParameters

---
+++ src/spacing/lib/prepareParameters/setDefaultKeyTimes.ts
@@ -0,0 +1,19 @@
+import { Parameters } from "../../interfaces";
+import { pipe } from "fp-ts/lib/pipeable";
+import { update } from "ramda";
+
+const initKeyTimesDefaultArray = (numOfKeyTimes: number): number[] =>
+  Array(numOfKeyTimes).fill(null, 0, numOfKeyTimes - 1);
+
+const setDefaultKeyTimes = (params: Parameters): Parameters =>
+  params.keyTimes
+    ? params
+    : {
+        ...params,
+        keyTimes: pipe(
+          update(0, 0, initKeyTimesDefaultArray(params.progression.length)),
+          update(-1, 1)
+        ),
+      };
+
+export default setDefaultKeyTimes;
