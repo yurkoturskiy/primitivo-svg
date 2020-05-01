de6126607b0f77b114cbc8cee1b226b03f178578
Yurko Turskiy <guandjoy@gmail.com>
2020-05-01 22:36:12 +0300

    Add oneToAll vertexesMethod

--- src/misc/vertexesMethods.ts
+++ src/misc/vertexesMethods.ts
@@ -2,3 +2,7 @@ export const setEach = <T>(...parameters: T[]) => (
   numOfVertexes: number
 ): T[] => [...parameters];
 
+// Fill all with one value
+export const oneToAll = (value: number | number[]) => (numOfVertexes: number) =>
+  Array(numOfVertexes).fill(value);
+
