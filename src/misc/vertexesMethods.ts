b36e80059a94c8f55f6205a4b04cc995e47edb3e
Yurko Turskiy <guandjoy@gmail.com>
2020-05-01 22:44:45 +0300

    Add randomRangePerEach vertexes method

--- src/misc/vertexesMethods.ts
+++ src/misc/vertexesMethods.ts
@@ -7,4 +7,11 @@ export const oneToAll = (value: number | [number, number]) => (
   numOfVertexes: number
 ) => Array(numOfVertexes).fill(value);
 
+// Create unique random values for each vertex
+export const randomRangePerEach = (min: number, max: number) => (
+  numOfVertexes: number
+): number[] =>
+  Array(numOfVertexes)
+    .fill(0)
+    .map(() => Math.random() * (max - min) + min);
 
