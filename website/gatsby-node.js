09ee6afcbb42a9a5a108582aa2fb9494058f1ccc
Yurko Turskiy <guandjoy@gmail.com>
2020-05-03 04:10:10 +0300

    Set up mdx documentation

--- website/gatsby-node.js
+++ website/gatsby-node.js
@@ -1,7 +1,47 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require("path")
 
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+  // Destructure the createPage function from the actions object
+  const { createPage } = actions
+
+  return new Promise((resolve, reject) => {
+    resolve(
+      graphql(
+        `
+          {
+            allMdx {
+              edges {
+                node {
+                  id
+                  frontmatter {
+                    path
+                  }
+                }
+              }
+            }
+          }
+        `
+      ).then(result => {
+        // this is some boilerlate to handle errors
+        if (result.errors) {
+          console.error(result.errors)
+          reject(result.errors)
+        }
+
+        // We'll call `createPage` for each result
+        result.data.allMdx.edges.forEach(({ node }) => {
+          createPage({
+            // This is the slug we created before
+            // (or `node.frontmatter.slug`)
+            path: node.frontmatter.path,
+            // This component will wrap our MDX content
+            component: path.resolve(`src/components/docs/template.js`),
+            // We can use the values in this context in
+            // our page layout component
+            context: { id: node.id },
+          })
+        })
+      })
+    )
+  })
+}
